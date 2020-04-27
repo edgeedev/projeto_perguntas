fa87643e8697251d43eb784000c456e9938f7869
dikdudu <dikdudu@hotmail.com>
2020-04-27 13:41:07 -0300

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_perguntas/questao.dart';
+import 'package:projeto_perguntas/resposta.dart';
 
-void main() => runApp(MyApp());
+main (){
+  runApp(AppPerguntas());
+}
 
-class MyApp extends StatelessWidget {
- 
+class AppPerguntas extends StatefulWidget {
+  @override
+  _AppPerguntasState createState() => _AppPerguntasState();
+}
+
+class _AppPerguntasState extends State<AppPerguntas> {  
+
+  var _perguntasRespondidas = 0;
+
+  void _responderPerguntas(){
+    
+    setState(() {
+        _perguntasRespondidas++;
+    });
+  
+  }
+
+  List perguntas = [
+    "Qual o sua cor preferida ?",
+    "Qual seu animal preferido?"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        primarySwatch: Colors.blue,
-      ),
-      home: Home(),
       debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Perguntas"),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: <Widget>[
+            Questao(perguntas[_perguntasRespondidas]),
+            Resposta("Resposta 1", _responderPerguntas),
+            Resposta("Resposta 2",_responderPerguntas),
+            Resposta("Resposta 3",_responderPerguntas),
+          ],
+        ),
+      ),
     );
-  }
-}
 
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-    );
   }
 }
